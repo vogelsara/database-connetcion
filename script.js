aa784462e1c869f1db47ce067ec5df6ce2b2876e
vogelsara <sari.vogel.94@gmail.com>
2019-02-21 10:50:47 +0100

    Add makeRequest function to the js file

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+function makeRequest(url, method, formdata, callback){
+    fetch(url, {
+        method: method,
+        body: formdata,
+    }).then((data) => {
+        return data.json()
+    }).then((result) => {
+        callback(result)
+    }).catch((err) => {
+        console.log(err)
+    })
+}
+
 function getAllStudents (){
-    
+
 }
